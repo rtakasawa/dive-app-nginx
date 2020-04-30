5fb71e622ce20b485c7a2b0a3ce72ae9574a772e
ryosuke takasawa <ten.r0619@gmail.com>
2020-04-30 22:10:27 +0900

    Nginxでのデプロイのためcommit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,10 +7,21 @@
 # before_fork do |server,worker|
 #   FileUtils.touch('/tmp/app-initialized')
 # end
-require 'fileutils'
-
+#
+# require 'fileutils'
+#
+# worker_processes 4
+# listen 'unix:///tmp/nginx.socket', backlog: 1024
+# before_fork do |server,worker|
+#   FileUtils.touch('/tmp/app-initialized')
+# end
+#
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
 worker_processes 4
 listen 'unix:///tmp/nginx.socket', backlog: 1024
+
 before_fork do |server,worker|
   FileUtils.touch('/tmp/app-initialized')
 end
